a0caf5d4832d7492b6fcc571708c69183f7c3895
lcs <cslee@itmoulds.com>
2021-01-05 15:48:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nuxt.js Memo Application
+## Welcome 
+ Nuxt.js 페이지 구성, 저장소, 동적 라우팅, 미들웨어 점검을 위한 프로젝트
+ 
+created by 이철승 2021.01.05
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container>
+    <h1>Nuxt.js로 Post 시작하기</h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
