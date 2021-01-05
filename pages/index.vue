f5d8958f4cc935949e193adf9a72413686ea4ec0
lcs <cslee@itmoulds.com>
2021-01-05 20:09:52 +0900

    메모 기록 버튼 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,48 @@
 <template>
   <b-container>
-    <h1>Nuxt.js로 Post 시작하기</h1>
+    <b-row>
+      <b-col
+        lg="4"
+        sm="12"
+      >
+        <b-card
+          title="최근 메모"
+        >
+
+        </b-card>
+      </b-col>
+      <b-col class="mt-sm-2 mt-lg-0">
+        <b-card
+          title="메모 작성"
+        >
+          <b-form>
+            <b-input-group>
+              <b-form-textarea
+                id="textarea"
+                placeholder="Enter something..."
+                rows="3"
+                max-rows="6"
+                no-resize
+              ></b-form-textarea>
+            </b-input-group>
+            <b-input-group class="mt-2">
+              <b-btn>기록</b-btn>
+            </b-input-group>
+          </b-form>
+        </b-card>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "index",
+  data() {
+    return {
+      text: '',
+    }
+  }
 }
 </script>
 
