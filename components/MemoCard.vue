5f70ca7c6340029a785c4e36a6a2a7d5a96dce12
lcs <cslee@itmoulds.com>
2021-01-06 16:06:26 +0900

    메모 카드 스타일 수정

--- components/MemoCard.vue
+++ components/MemoCard.vue
@@ -4,6 +4,14 @@
       <b-card-body>
         <b-card-title>
           로스군
+          <b-btn
+              pill
+              type="button"
+              variant="white"
+              class="float-right px-2"
+          >
+            <b-icon-three-dots color="black"></b-icon-three-dots>
+          </b-btn>
         </b-card-title>
         <b-card-sub-title>
           2021-01-06
@@ -13,31 +21,32 @@
         </b-card-text>
       </b-card-body>
       <b-list-group flush>
-        <b-list-group-item>
+        <b-list-group-item class="py-0">
           <b-button-group
               class="d-flex"
-              size="sm"
+              size="md"
           >
             <b-button
                 variant="white"
                 type="button"
             >
               <b-icon-hand-thumbs-up color="black"></b-icon-hand-thumbs-up>
-              좋아요
+              &nbsp;좋아요
+            </b-button>
+            <b-button
+                variant="white"
+                type="button"
+                class="d-flex align-items-center justify-content-center justify-items-center align-content-center"
+            >
+              <b-icon-chat-left color="black"></b-icon-chat-left>
+              &nbsp; 댓글쓰기
             </b-button>
             <b-button
                 variant="white"
                 type="button"
             >
-              <b-icon-chat-left-text color="black"></b-icon-chat-left-text>
-              댓글쓰기
-            </b-button>
-            <b-button
-                variant="white"
-                type="button"
-            >
-              <b-icon-three-dots color="black"></b-icon-three-dots>
-              메뉴
+              <b-icon-share-fill color="black"></b-icon-share-fill>
+              &nbsp; 공유하기
             </b-button>
           </b-button-group>
         </b-list-group-item>
